00fb8fdb1352c3a16953ff5bf955ecc41dc9856e
Vinicius Augutis <viniciusaugutis@gmail.com>
2021-02-08 21:29:17 -0300

    Botao de esqueceu sua senha

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -16,20 +16,23 @@ class LoginPage extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Container(
-                  height: 450,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: [
-                      new BoxShadow(
-                        color: Colors.black12,
-                        offset: new Offset(1, 2.0),
-                        blurRadius: 5,
-                        spreadRadius: 1,
-                      )
-                    ],
-                  ),
-                  child: ListView(children: <Widget>[
-                    Row(
+                height: 480,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black12,
+                      offset: new Offset(1, 2.0),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                    )
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: ListView(
+                    children: <Widget>[
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           Column(children: <Widget>[
@@ -44,8 +47,56 @@ class LoginPage extends StatelessWidget {
                             child: Text("Sign up"),
                             onPressed: () {},
                           )
-                        ]),
-                  ])),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16)),
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        autofocus: true,
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16)),
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        height: 40,
+                        child: FlatButton(
+                          child: Text("Forgot your password?"),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
