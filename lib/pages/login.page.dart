960a4b52b6a8aa3ce9c36663e60bbbf519f6d239
Vinicius Augutis <viniciusaugutis@gmail.com>
2021-02-08 21:17:58 -0300

    Utilizando row e column para alinhar widgets

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -17,13 +17,34 @@ class LoginPage extends StatelessWidget {
             children: <Widget>[
               Container(
                   height: 450,
-                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                    new BoxShadow(
-                      color: Colors.black12,
-                      offset: new Offset(1, 2.0),
-                      blurRadius: 5,
-                      spreadRadius: 1,
-                    )
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.black12,
+                        offset: new Offset(1, 2.0),
+                        blurRadius: 5,
+                        spreadRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: ListView(children: <Widget>[
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(children: <Widget>[
+                            Text("Welcome",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500,
+                                )),
+                            Text("Sign in to continue"),
+                          ]),
+                          FlatButton(
+                            child: Text("Sign up"),
+                            onPressed: () {},
+                          )
+                        ]),
                   ])),
             ],
           ),
